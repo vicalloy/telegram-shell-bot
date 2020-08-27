9791b8812175251277bcfd311d9e6f9de47da8e1
oldherl <oldherl@gmail.com>
2020-08-28 00:17:49 +0800

    fix typo. missing letter T.

--- bot.py
+++ bot.py
@@ -128,7 +128,7 @@ def __check_cmd(cmd: str):
     if cmd.startswith('sudo'):
         cmd = cmd[4:].strip()
     cmd = cmd.split(' ')[0]
-    if settings.CMD_WHITE_LIS and cmd not in settings.CMD_WHITE_LIST:
+    if settings.CMD_WHITE_LIST and cmd not in settings.CMD_WHITE_LIST:
         return False
     if cmd in settings.CMD_BLACK_LIST:
         return False
